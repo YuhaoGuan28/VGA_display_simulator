123bb177dd93ad7540fd8fe76d8ebcbc80bb5174
Yuhao Guan <94421120+YuhaoGuan28@users.noreply.github.com>
2025-04-07 21:17:58 -0400

    1

---
+++ VGA simulator/some examples/FONT12/J.vhd
@@ -0,0 +1,14 @@
+type gui_ele_type is array (0 to 10, 0 to 7) of std_logic_vector(11 downto 0);
+    constant J_12 : gui_ele_font12_type := (
+        (x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00"),
+        (x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00"),
+        (x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00"),
+        (x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00"),
+        (x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00"),
+        (x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00"),
+        (x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00"),
+        (x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00"),
+        (x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00"),
+        (x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00"),
+        (x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00", x"F00")
+    );
